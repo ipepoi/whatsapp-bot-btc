1bf05faf47e82b272eb70dcd05f63d2aa4683f82
ipepoi <imadupoi@gmail.com>
2021-03-19 09:52:27 +0700

    Add index and basic bot

---
+++ index.js
@@ -0,0 +1,22 @@
+const { Client } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+const client = new Client();
+
+client.on('qr', (qr) => {
+    // Generate and scan this code with your phone
+    qrcode.generate(qr,{small: true}, function (qrcode) {
+    console.log(qrcode);
+    });
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('message', msg => {
+    if (msg.body == '!ping') {
+        msg.reply('pong');
+    }
+});
+
+client.initialize();
